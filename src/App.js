605589c75241f00eb93b9d632ab16d19406254b8
sergiorioss-dul <59352543+sergiorioss-dul@users.noreply.github.com>
2022-07-19 20:04:00 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import CalceProduct from "./components/CalceProduct";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <CalceProduct />
+    </div>
+  );
+}
---
+++ src/components/CalceProduct.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./calceProduct.sass";
+
+const CalceProduct = () => {
+  return (
+    <div>
+      <h1>Producto Calcetose 2022</h1>
+    </div>
+  );
+};
+
+export default CalceProduct;
